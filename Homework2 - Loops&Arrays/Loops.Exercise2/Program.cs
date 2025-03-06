6a70b394b0d538b2b14e45e0f2d5d4491dbfa6c9
AnaMarjanovik <108395368+AnaMarjanovik@users.noreply.github.com>
2025-03-06 03:21:24 +0100

    Add files via upload

---
+++ Homework2 - Loops&Arrays/Loops.Exercise2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Enter the first input number.");
+int input1 = int.Parse(Console.ReadLine());
+
+for(int i=2; i<=input1; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+
+Console.WriteLine("Enter the second input number.");
+int input2 = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= input2; i++)
+{
+    if (i % 2 != 0)
+    {
+        Console.WriteLine(i);
+    }
+}
---
+++ Homework2 - Loops&Arrays/Loops.Exercise3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter the input!");
+int input = int.Parse(Console.ReadLine());
+
+if (input < 100)
+{
+    for (int i = 1; i <= input; i++)
+    { 
+        if (i % 3 != 0 && i % 7 != 0)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+else
+{
+    Console.WriteLine("The limit is reached and stop counting");
+}
