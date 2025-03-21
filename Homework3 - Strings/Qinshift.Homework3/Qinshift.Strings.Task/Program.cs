a492bdf518fcb173e85f0d0924b19102b3cad189
AnaMarjanovik <108395368+AnaMarjanovik@users.noreply.github.com>
2025-03-21 23:14:36 +0100

    Add files via upload

---
+++ Homework3 - Strings/Qinshift.Homework3/Qinshift.Strings.Task/Program.cs
@@ -0,0 +1,33 @@
+﻿static void AgeCalculator()
+{
+    Console.WriteLine("Enter your birthday using the following format (dd/mm/yyy)");
+
+    string birthday = Console.ReadLine();
+
+    DateTime convertedBirthday = DateTime.Parse(birthday);
+
+    DateTime currentDate = DateTime.Today;
+
+    int age;
+    Console.WriteLine("Has your birthday passed?");
+    string answer = Console.ReadLine();
+    if (answer.ToUpper() == "YES")
+    {
+        age = currentDate.Year - convertedBirthday.Year;
+        Console.WriteLine($"You are {age} years old!");
+    }
+    else if (answer.ToUpper() == "NO")
+    {
+        age = currentDate.Year - convertedBirthday.Year;
+        Console.WriteLine($"You are currently {age - 1} years old but after a few months, you will be {age} years old!");
+    }
+    else
+    {
+        Console.WriteLine("You enter an invalid answer !!!");
+    }
+
+
+
+}
+
+AgeCalculator();
