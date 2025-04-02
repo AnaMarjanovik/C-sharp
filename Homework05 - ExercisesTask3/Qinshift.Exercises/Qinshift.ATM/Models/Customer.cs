e8c03b4daf5df721685dc65ca84b1108d1c89628
AnaMarjanovik <108395368+AnaMarjanovik@users.noreply.github.com>
2025-04-03 00:01:21 +0200

    Add files via upload

---
+++ Homework05 - ExercisesTask3/Qinshift.Exercises/Qinshift.ATM/Models/Customer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qinshift.ATM.Models
+{
+    public class Customer
+    {
+        public Customer()
+        {
+            
+        }
+
+        public Customer(string firstName,string lastName,string cardNumber, string pin, int money)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            CardNumber = cardNumber;
+            Pin = pin;
+            Money = money;
+            
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CardNumber { get; set; }
+        public string Pin { get; set; }
+        public int Money { get; set; }
+
+
+        public bool checkIfExist(string inputCardNumber, string inputPin)
+        {
+            if(CardNumber == inputCardNumber && Pin == inputPin)
+            {
+              
+                    Console.WriteLine($"Welcome {FirstName} {LastName}!");
+                    return true;
+            }
+            return false;
+        }
+
+        public void customerChoice(int inputChoice)
+        {
+            switch (inputChoice)
+            {
+                case 1:
+                    Console.WriteLine($"Current balance of money on your account is {Money}$.");
+                    break;
+                case 2:
+                    Console.WriteLine("Enter how much money you want to withdraw?");
+                    int withdraw = int.Parse(Console.ReadLine());
+                    if(Money > withdraw)
+                    {
+                        Console.WriteLine($"You withdrew {withdraw}$. You have {Money - withdraw}$ left on your account.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You don't have enough money to withdraw.");
+                    }
+                        break;
+                case 3:
+                    Console.WriteLine("Enter how much money you want to deposit.");
+                    int deposit = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"You deposit {deposit}$. You have {Money + deposit}$ on your account.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
+
+        
+    }
+}
---
+++ Homework05 - ExercisesTask3/Qinshift.Exercises/Qinshift.ATM/Program.cs
@@ -0,0 +1,60 @@
+﻿
+
+// ATM APP
+
+using Qinshift.ATM.Models;
+
+Customer customer1 = new Customer("Ana", "Marjanovik", "1234-1234-1234-1234", "1234", 600);
+Customer customer2 = new Customer("Ivica", "Rodikj", "5678-5678-5678-5678", "5678", 700);
+Customer customer3 = new Customer("Dimitar", "Stojkov", "9101-9101-9101-9101", "9101", 800);
+
+Console.WriteLine("Welcome to the ATM app !");
+
+Console.WriteLine("Please enter yor card number. (xxx-xxx-xxx-xxx)");
+string cardNumber = Console.ReadLine();
+
+Console.WriteLine("Enter Pin:");
+string pin = Console.ReadLine();
+
+
+Customer authenticatedCustomer = null;
+
+if (customer1.checkIfExist(cardNumber, pin))
+{
+    authenticatedCustomer = customer1;
+}
+else if (customer2.checkIfExist(cardNumber, pin))
+{
+    authenticatedCustomer = customer2;
+}
+else if (customer3.checkIfExist(cardNumber, pin))
+{
+    authenticatedCustomer = customer3;
+}
+
+
+Console.WriteLine("What would you like to do:");
+Console.WriteLine("1.Check Balance \n2.Cash Withdrawal \n3.Cash Deposit");
+
+int choice;
+if (int.TryParse(Console.ReadLine(), out choice))
+{
+    authenticatedCustomer.customerChoice(choice); 
+}
+else
+{
+    Console.WriteLine("Invalid input.");
+}
+
+Console.WriteLine("Thank you for using the ATM app.");
+
+
+
+
+
+
+
+
+
+
+
